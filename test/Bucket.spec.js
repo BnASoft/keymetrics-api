31883030d582512e6bb05702e732d922e13028f4
Aymeric Lavit d'Hautefort <alavit-d@student.42.fr>
2016-09-08 17:04:39 +0200

    basic bucket calls testing

---
+++ test/Bucket.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var should     = require('should');
+var expect     = require('expect');
+
+var km = require('../lib/keymetrics')(process.env.TEST_TOKEN, process.env.TEST_ID);
+var Bucket = require('../lib/Bucket');
+var bucket = km.bucket;
+
+describe('Bucket', function() {
+  describe('Bucket initialization', function() {
+    it ('Should null when incorrect id', function() {
+      var tmp = Bucket('WRONG', 'http://localhost', '1212');
+      should.not.exist(tmp);
+    });
+  });
+
+  describe('Basic functions', function() {
+    it ('Should fetch user role', function(done) {
+      bucket.fetchUserRole((err, res) => {
+        should.not.exist(err);
+        res.should.be.equal('owner');
+        done();
+      });
+    });
+
+    it ('Should get test bucket and populate current_raw', function(done) {
+      bucket.get((err, res) => {
+        should.not.exist(err);
+        res._id.should.be.equal(bucket.current_raw._id);
+        done();
+      });
+    });
+  });
+});
