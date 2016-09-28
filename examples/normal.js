f9ecffb42ae7cae90b6721159a632dcb608ce7e8
vmarchaud <thisismac47@gmail.com>
2016-09-28 14:47:15 +0200

    refactor design of the wrapper

--- examples/normal.js
+++ examples/normal.js
@@ -1,29 +1,12 @@
-var Keymetrics = require('./lib/keymetrics');
+var Keymetrics = require('../lib/keymetrics');
 
 var km = new Keymetrics({
-  token: "amkwpkvd5x8vca3g0t1d8wfklkmq51sesvxb6aaxs6u674qzlreu6k429b5lbmdx" 
+  refresh_token: "eim9bea23p21l7nfuvyas2vf1yug35vw70kd9o245o3ekgb6a1bp4fm3lxgsxug6",
+  token_type: 'refresh_token'
+}, function (err, data) {
+  console.log(err || data);
 });
 
-km.init("5fqz8bdggabnxdt", function(err, lel) {
-
-  km.bucket.fetchUserRole(function(err, res) {
-    console.log(res);
-  });
-
-  km.realtime.init(() => {
-  }, (data) => {
-    console.log(data);
-  });
-  
-  km.bucket.getPlans((err, res) => {
-    console.log(res);
-  });
-
-  km.bucket.getMetaServers((err, res) => {
-    console.log(res);
-  });
-    
-  setTimeout(() => {
-    km.realtime.close();
-  }, 10000);
-});
+km.bus.on('error:*', function (err) {
+  console.log(err);
+})
